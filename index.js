17d37bb834e14f0f2b38be7f5977fc686f135121
Prashant Kumar Singh <pk3076889@gmail.com>
2022-06-24 09:38:33 +0530

    comments

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
+// if player 2 wins
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
+// if there is draw
 else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
